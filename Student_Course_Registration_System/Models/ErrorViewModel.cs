103fd983102e6fea1d2024069ae81e85f65cb184
shubham <gagareshubham0@gmail.com>
2025-08-15 15:23:29 +0530

    Add project files.

---
+++ Student_Course_Registration_System/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Student_Course_Registration_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
